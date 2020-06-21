e2e99c493f630c56e8bba8cdca8b5aac01816f40
David Cusi <robodavcuqupe@gmail.com>
2020-06-21 18:14:37 -0400

    Agregado las paginas de web-irpa.html y asai-mate.html

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,11 @@
-$blur = document.getElementById('blur');
-$burgerBtn = document.getElementById('burger-btn');
-$menu = document.getElementById('menu');
-$body = document.getElementById('body');
+const $blur = document.getElementById('blur');
+const $burgerBtn = document.getElementById('burger-btn');
+const $menu = document.getElementById('menu');
+const $body = document.getElementById('body');
 
 $burgerBtn.addEventListener('click', () => {
-    // $blur.classList.toggle('blur--active');
-    $burgerBtn.classList.toggle('burger-btn--active');
-    $menu.classList.toggle('menu--active');
-    $body.classList.toggle('overflow--hidden');
+  $blur.classList.toggle('blur--active');
+  $burgerBtn.classList.toggle('burger-btn--active');
+  $menu.classList.toggle('menu--active');
+  $body.classList.toggle('overflow--hidden');
 });
